2c3e3f2460f8582160491512d0c7ec12f4bcf68c
Mohamed Emad <mohamed.elsherbiny215@gmail.com>
2022-04-29 11:43:19 +0200

    edit files

--- sanity.js
+++ sanity.js
@@ -4,7 +4,7 @@ import createImageUrlBuilder from '@sanity/image-url'
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  apiVersion: '2021-10-21',
+  apiVersion: '2022-04-01',
 
   useCdn: process.env.NODE_ENV === 'production',
 }
